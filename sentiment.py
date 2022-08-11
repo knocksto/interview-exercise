6598e994c94d0366fd0da4d7e1a840dc94bef249
Vincent Tetteh <vincent.tetteh@turntabl.io>
2022-08-11 14:56:54 +0000

    created sentiment analyzer

---
+++ sentiment.py
@@ -0,0 +1,52 @@
+
+from enum import Enum
+import enum
+import math
+from typing import List
+
+import pytest
+# from textblob import TextBlob
+import re
+from collections import Counter
+
+class Expression(enum.Enum):
+    NEGATIVE = "NEGATIVE"
+    POSITIVE = "POSITIVE"
+    NEUTRAL = "NEUTRAL"
+
+def sentiment_analyzer(text:str, wordList:List[str]):
+    total_text = len(text)
+    text_list = re.split('[ !.,]', text)
+    text_dict = dict(Counter(text_list))
+    new_text_dict = {}
+    score = 0
+    text_with_scores = {}
+    
+    for key, value in text_dict.items():
+        if len(key) > 3:
+            new_text_dict[key] = value
+
+    for key, value in new_text_dict.items():
+        if key in wordList:
+            if value == 1:
+                score = (value / total_text) * 100
+                print(value)
+                return Expression.POSITIVE.name
+            if value > 1:
+                text_with_scores[key] = value
+
+    for key, value in text_with_scores.items():
+        if value == 2:
+            return Expression.NEUTRAL.name
+        else:
+            return Expression.NEGATIVE.name
+
+    return -1
+    
+    # print(new_text_dict)
+    # blob = TextBlob(text)
+    # print(list(blob.sentiment)[0])
+    # print(text_with_scores)
+
+
+print(sentiment_analyzer("The world is a terrible place to live in terrible.", ["ugly","terrible"]))
---
+++ test_sentiment.py
@@ -0,0 +1,27 @@
+import sys
+
+# from pywin.mfc.object import Object
+from testplan import test_plan
+from testplan.testing.multitest import MultiTest, testcase, testsuite
+from sentiment import sentiment_analyzer
+
+
+@testsuite
+class SentimentTest(object):
+    text = "The world is a ugly place to live in terrible. ugly! terrible"
+    worldList = ["ugly"]
+
+    @testcase
+    def test_negative(self, env, result):
+        result.equal(sentiment_analyzer("The world is a ugly place to live in terrible. ugly! terrible", ["ugly"]), "NEGATIVE", description='Passing assertion')
+        result.equal(sentiment_analyzer("The world is a ugly place to live in terrible. terrible", ["ugly"]), "po", description='Failing assertion')
+
+
+@test_plan(name='Sentiment')
+def main(plan):
+    test = MultiTest(name='SentimentTest', suites=[SentimentTest()])
+    plan.add(test)
+
+
+if __name__ == '__main__':
+    sys.exit(not main)
