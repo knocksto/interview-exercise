84fb9df5f38cebb9e04a83ee8d0331400cd74b1c
Vincent Tetteh <vincent.tetteh@turntabl.io>
2022-08-11 18:22:00 +0000

    refactored code

--- sentiment.py
+++ sentiment.py
@@ -15,39 +15,34 @@ class Expression(enum.Enum):
     NEUTRAL = "NEUTRAL"
 
 def sentiment_analyzer(text:str, wordList:List[str]):
-    
+
     total_text = len(text)
-    text_list = re.split('[ !.,]', text)
+    print(total_text)
+    text_list = re.split('[ !.,]', text.lower())
     text_dict = dict(Counter(text_list))
     new_text_dict = {}
     score = 0
+    expression_dectector = ""
     text_with_scores = {}
     
     for key, value in text_dict.items():
-        if len(key) > 3:
+        if len(key) >= 3:
             new_text_dict[key] = value
 
     for key, value in new_text_dict.items():
         if key in wordList:
-            if value == 1:
-                score = (value / total_text) * 100
-                print(value)
-                return Expression.POSITIVE.name
-            if value > 1:
-                text_with_scores[key] = value
-
-    for key, value in text_with_scores.items():
-        if value == 2:
-            return Expression.NEUTRAL.name
-        else:
-            return Expression.NEGATIVE.name
-
-    return -1
+            score = (value / total_text) * 100
+            print(score)
+            print(value)
+            if value >= 1 and score > 1:
+                return Expression.NEGATIVE.name
+            elif value and score >= 0.6:
+                  return Expression.POSITIVE.name
+            elif value and score <= 0.6:
+                return Expression.NEUTRAL.name
     
-    # print(new_text_dict)
-    # blob = TextBlob(text)
-    # print(list(blob.sentiment)[0])
-    # print(text_with_scores)
+    print(score)
+    return -1
 
 
-print(sentiment_analyzer("The world is a terrible place to live in terrible.", ["ugly","terrible"]))
+print(sentiment_analyzer("The world is a terrible place to live in. Terrible!", ["sad","terrible","angry","mad","bad","ugly","awful","stupid"]))
