794e4f7db65914d11467196b567e7fda1bae0257
amin-io <amin.hadi@turntabl.io>
2022-08-12 09:49:25 +0000

    added test

--- sentiment.py
+++ sentiment.py
@@ -1,18 +1,9 @@
 import string
 
-
 negative_words = ["bad", "ugly", "terrible", "awful", "stupid", "mad", "angry", "sad"]
 counter = 0
 
 def sentiment_analysis(sentence: str) -> str:
-    if calPercentage(sentence) <= 5:
-        return "POSITIVE"
-    elif calPercentage(sentence) < 20:
-        return "NEUTRAL"
-    else:
-        return "NEGATIVE"
-
-def negative_words_count(sentence: str) -> int:
     global counter
     split_sentence = sentence.lower().translate(str.maketrans('', '', string.punctuation)).split(" ")
 
@@ -20,14 +11,17 @@ def negative_words_count(sentence: str) -> int:
         for word in split_sentence:
             if neg_word == word:
                 counter = counter + 1
-    return counter
-    
-def calPercentage(sentence: str) -> int:
-    split_sentence = sentence.lower().translate(str.maketrans('', '', string.punctuation)).split(" ")
 
     word_counter = sum(len(word) >= 3 for word in split_sentence)
 
-    return negative_words_count(sentence) / word_counter * 100
+    neg_word_percentage = counter / word_counter * 100
+
+    if neg_word_percentage <= 5:
+        return "POSITIVE"
+    elif neg_word_percentage < 20:
+        return "NEUTRAL"
+    else:
+        return "NEGATIVE"
 
 if __name__ == '__main__':
-    print(sentiment_analysis("I'm going shopping and I'm super excited. The terrible thing is that the boots I wanted are no longer in sale, which makes me a bit angry, because I now have to pay full price."))
+    print(sentiment_analysis(2))
